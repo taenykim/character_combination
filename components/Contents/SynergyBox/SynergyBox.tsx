285a4e418c420b073c45ba2bb0c206d1660f90a9
taeeun kim <kimtaeeeny@gmail.com>
2020-04-22 04:29:55 +0900

    시너지 박스 생성

--- components/Contents/SynergyBox/SynergyBox.tsx
+++ components/Contents/SynergyBox/SynergyBox.tsx
@@ -3,17 +3,27 @@ import styled from 'styled-components'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../../reducers'
 
-const languages = { KOR: { synergyBox: '시너지 박스' }, ENG: { synergyBox: 'Synergy Box' } }
+const languages = { KOR: { synergyBox: '시너지' }, ENG: { synergyBox: 'Synergy' } }
 
 const Container = styled.div`
   color: white;
   width: 800px;
   margin: 20px 0px 20px 0px;
+  font-size: 15px;
 `
+
+const Title = styled.span``
+
+const Synergy = styled.span``
 
 const SynergyBox = () => {
   const language = useSelector((state: RootState) => state.wrapper.language)
-  return <Container>{languages[language].synergyBox}</Container>
+  return (
+    <Container>
+      <Title>{languages[language].synergyBox}</Title>
+      <Synergy>없음</Synergy>
+    </Container>
+  )
 }
 
 export default SynergyBox
